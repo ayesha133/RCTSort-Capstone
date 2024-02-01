35271c025328d2828ea9f34b861da9358e5b598f
ayesha133 <ayesharshad88@gmail.com>
2024-01-31 21:13:41 -0500

    add code to read results from image

--- yoloMohammad.py
+++ yoloMohammad.py
@@ -24,12 +24,41 @@ def save_frame_camera_key(device_num, dir_path, base_path, ext='jpg', delay=1, w
 
     cv2.destroyWindow(window_name)
 
-save_frame_camera_key(1, dir_path, base_path) # to test my code, replace the first parameter from 1 to 0 on windows, cuz Mac is dumb
+save_frame_camera_key(0, dir_path, base_path) # to test my code, replace the first parameter from 1 to 0 on windows, cuz Mac is dumb
 
 model = YOLO("yolo-Weights/yolov8n.pt")
 
 results = model('img/camera_capture_1.jpg')
 
-print(results)
+#THIS IS USED TO STORE THE IMAGE MACHINE LEARNING ALGORITHM RESULTS IN OUR OWN ARRAY FOR DATA MANIPULATION
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+newResult = []
+
+for r in results:
+    print(f"HELLOOOO {r}")
+    boxes = r.boxes
+
+    for box in boxes:
+        cls = int(box.cls[0])
+        print("Class name -->", classNames[cls])
+
+        newResult.append(classNames[cls])
+
+print(f"new results {newResult}")
+
+
+
+
 
 
